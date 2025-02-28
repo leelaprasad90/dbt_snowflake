4d22511638e6c62bea350b983430ffae0e98d27e
Leelaprasad Nuvvula <leelaprasad90@gmail.com>
2025-02-28 13:28:50 +0000

    macro update

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -1,7 +1,7 @@
 {% macro generate_custom_schema() %}
     {% if target.name == 'prod' %}
-        'HOURLY_ANALYTICS'
+        HOURLY_ANALYTICS
     {% else %}
-        'DEV_HOURLY_ANALYTICS'
+        DEV_HOURLY_ANALYTICS
     {% endif %}
 {% endmacro %}
